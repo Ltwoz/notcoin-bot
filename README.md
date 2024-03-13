64e89ebd74a64bae3db838e66e9fb109fab39975
Ltwoz <warrapat.102@gmail.com>
2024-03-13 19:27:34 +0700

    one commit 🚀

---
+++ README.md
@@ -0,0 +1,42 @@
+<p align="center">
+  <br>
+  <img width="250" src="logo.png">
+  <br>
+</p>
+
+<h1 align="center">
+  Notcoin Auto Clicker
+</h1>
+
+<h4 align="center">
+    Small script to automatically clicks on coin, collect rockets and use available boosts.
+    <br/>Works well in one session, it will refresh the page if click request is failed or page is not loaded. You can change a timeouts by changing variables if you need.
+</h4>
+
+## Run Notcoin in browser
+
+This method allows you to use only one session (you don't need to recreate it everytime)
+
+1. Open [Telegram Web](https://web.telegram.org)
+2. Open [Notcoin Bot](https://web.telegram.org/k/#@notcoin_bot)
+3. Open Dev Tools (`Command+Option+I / F12 or Control+Shift+I`)
+4. Press `Command+O / Control+O` and open file `telegram-web-app.js`.
+5. In file find string `Object.defineProperty(WebApp, 'platform',`, it should be on 1325 position
+6. Change `return webAppPlatform;` to `return 'ios';` and [save for overrides](https://senuravihanjayadeva.medium.com/local-overrides-in-chrome-devtools-f4a148de30c2#:~:text=Locate%20the%20file%20you%20want,overrides%E2%80%9D%20from%20the%20context%20menu.) this file
+7. Refresh the page and try to open Notcoin Bot
+
+If you can see the coin and doesn't see an message that you should use mobile then everything is ok
+
+### Run autoclicker
+
+1. Install [Violentmonkey](https://violentmonkey.github.io/)
+2. Open Dev Tools (`Command+Option+I / F12 or Control+Shift+I`)
+3. Add script from [index.js](https://github.com/Ltwoz/notcoin-bot/blob/main/index.js) into Violentmonkey
+4. Open [Notcoin Bot](https://web.telegram.org/k/#@notcoin_bot)
+5. Refresh the page
+
+The script should automatically open bot and reload it if needed. If something breaks, just try to reload the page.
+
+### Script
+
+You can find it [here in index.js](https://github.com/Ltwoz/notcoin-bot/blob/main/index.js)
---
+++ index.js
@@ -0,0 +1,194 @@
+// ==UserScript==
+// @name        notcoin-bot
+// @namespace   Violentmonkey Scripts
+// @match       https://*.telegram.org/*#@notcoin_bot
+// @match       https://*.joincommunity.xyz/*
+// @grant       GM_addValueChangeListener
+// @grant       GM_removeValueChangeListener
+// @grant       GM_setValue
+// @version     1.0
+// @author      Ltwoz
+// @homepageURL https://github.com/Ltwoz/notcoin-bot
+// ==/UserScript==
+
+// === helpers ===
+const delay = (time) => new Promise((r) => setTimeout(r, time));
+const getEvent = () => ({
+    touches: [
+        {
+            clientX: Math.random() * 250 + 52,
+            clientY: Math.random() * 582 + 512,
+        },
+    ],
+});
+const getReactProps = (component) =>
+    component
+        ? Object.entries(component).find(([key]) =>
+              key.startsWith("__reactProps$")
+          )[1]
+        : null;
+const getIdleTime = () => 5 * 60 * 1000;
+const getRandomClickTime = () => Math.floor(Math.random() * 142 + 12);
+const waitForElm = (selector, wait = 0) => {
+    return new Promise((resolve, reject) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver((mutations) => {
+            if (document.querySelector(selector)) {
+                clearTimeout(timeout);
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        const timeout =
+            wait &&
+            setTimeout(() => {
+                observer.disconnect();
+                reject("Never showed up. " + selector);
+            }, wait);
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+};
+
+// === script ===
+let shouldCheckBoosts = false;
+let intervalId;
+let clicksCount = 0;
+
+const MIN_SCORE = 100;
+const MAX_CLICKS_PER_ITERATION = 1000;
+
+const coinClick = async () => {
+    const coin = document.querySelector('div[class^="_notcoin"]');
+    const scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
+    const score = scoreElement ? parseInt(scoreElement.textContent) : null;
+
+    // Rocket click
+    const rocket = document.querySelector('img[class^="_root"]');
+    const rocketProps = getReactProps(rocket);
+    if (rocketProps) rocketProps.onClick();
+
+    if (intervalId === null || score === null) return;
+    const isMinScore = score < MIN_SCORE;
+    const isMaxClicksForIteration = clicksCount < MAX_CLICKS_PER_ITERATION;
+    const shouldClick = !isMinScore && isMaxClicksForIteration;
+
+    // Coin click
+    if (shouldClick && coin) {
+        clicksCount++;
+        const { onTouchStart, onTouchEnd } = getReactProps(coin);
+        onTouchStart(getEvent());
+        await delay(80);
+        onTouchEnd();
+    } else {
+        clearInterval(intervalId);
+        clicksCount = 0;
+        console.info("%c INFO: idle time", "color: #64b5f6");
+        if (isMinScore && shouldCheckBoosts) boostClick();
+        await delay(getIdleTime());
+        console.info("%c INFO: trying again", "color: #64b5f6");
+        start();
+    }
+};
+
+const start = () => {
+    intervalId = setInterval(coinClick, getRandomClickTime());
+};
+const stop = () => {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+const BOOST_CLICK_TIMEOUT = 2500;
+
+const boostClick = async () => {
+    console.info("%c INFO: open boosts", "color: #64b5f6");
+    const buttonGroup = document.querySelector('div[class^="_buttonGroup"]');
+    const boostButton = buttonGroup && getReactProps(buttonGroup.lastChild);
+    await delay(BOOST_CLICK_TIMEOUT);
+    if (!boostButton) return;
+    boostButton.onClick();
+    await delay(BOOST_CLICK_TIMEOUT);
+    const dailyTask = document.querySelector(
+        'div[class^="_taskDailyItem"]:not([class*="_completed"])'
+    );
+    const dailyBoost = getReactProps(dailyTask);
+    const taskCarousel = document.querySelector(
+        'div[class^="_taskCarousel"][class*="_willChange"]'
+    );
+    const boost = getReactProps(
+        taskCarousel &&
+            taskCarousel.querySelector('div[class*="_rippleEffect"]')
+    );
+    const boosterButton = dailyBoost || boost;
+    if (boosterButton) {
+        boosterButton.onClick();
+        await delay(BOOST_CLICK_TIMEOUT);
+        const getButton = getReactProps(
+            document.querySelector('button[class*="_typeBlue"]')
+        );
+        if (getButton) getButton.onClick();
+        await delay(BOOST_CLICK_TIMEOUT);
+        console.info("%c DONE: boosted", "color: #bada55");
+    }
+    console.info("%c INFO: close boosts", "color: #64b5f6");
+    history.back();
+};
+
+const init = async () => {
+    console.info("%c INIT: start initialization", "color: #64b5f6");
+    await waitForElm(".reply-markup-button").then(() => {
+        const button = document.querySelector(".reply-markup-button");
+        button.click();
+        console.info("%c INIT: initializated", "color: #64b5f6");
+    });
+};
+
+if (location.host === "web.telegram.org") {
+    init();
+    GM_addValueChangeListener("shouldRefresh", (_name, _oldValue, newValue) => {
+        console.info("%c INFO: refreshing", "color: #64b5f6");
+        if (newValue) location.reload();
+    });
+}
+
+if (location.host === "clicker.joincommunity.xyz") {
+    waitForElm('div[class^="_notcoin"]', 10000)
+        .then(start)
+        .catch(() => {
+            console.info(
+                "%c INFO: not started. reload window",
+                "color: #64b5f6"
+            );
+            location.reload();
+        });
+
+    GM_setValue("shouldRefresh", false);
+
+    const requestRefresh = () => {
+        console.info("%c INFO: click request failed", "color: #64b5f6");
+        GM_setValue("shouldRefresh", true);
+    };
+
+    const { fetch: origFetch } = window.unsafeWindow;
+    window.unsafeWindow.fetch = async (...args) => {
+        try {
+            const response = await origFetch(...args);
+            if (response && response.status === 401) requestRefresh();
+            return response;
+        } catch (error) {
+            if (!args[0].signal.aborted) {
+                requestRefresh();
+            }
+            throw error;
+        }
+    };
+}
+ 
